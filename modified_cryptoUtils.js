04741e461cfced99eed3e9a3453f50eb09566d85
surya-githubb <108220723+surya-githubb@users.noreply.github.com>
2025-08-13 13:04:07 +0530

    Update modified_cryptoUtils.js

--- modified_cryptoUtils.js
+++ modified_cryptoUtils.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 
 function encryptData(data, secretKey) {
     // crypto.randomBytes(16);
-  const sameIv = 'a93080f6d06bfa54bf30035b8cf19380'
+  const sameIv = 'a93080f6d06bfa54bf30035b8cf19380' // store in .env
   const iv = Buffer.from(sameIv, 'hex');
   const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
   let encrypted = cipher.update(data, 'utf-8', 'hex');
@@ -11,7 +11,7 @@ function encryptData(data, secretKey) {
 }
 
 function decryptData(encryptedData, secretKey) {
-  const sameIv = 'a93080f6d06bfa54bf30035b8cf19380'
+  const sameIv = 'a93080f6d06bfa54bf30035b8cf19380' // store in .env
   const iv = Buffer.from(sameIv, 'hex'); // Convert the IV back to a binary buffer from hex.
   const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(secretKey, 'hex'), iv);
   let decrypted = decipher.update(encryptedData, 'hex', 'utf-8');
@@ -20,7 +20,7 @@ function decryptData(encryptedData, secretKey) {
 }
 
 // crypto.randomBytes(32);
-// secretKey = 'b489bef6477f82e9166f74f79a1f86cd1808c80057f3ee4ad39f158b5728ec39'
+// secretKey = 'b489bef6477f82e9166f74f79a1f86cd1808c80057f3ee4ad39f158b5728ec39' // store in .env
 // console.log(encryptData('password', secretKey))
 // console.log(decryptData('db44bff322087d75c1f49da678a426af', secretKey))
 
